504c48edd2a61cc355536e2583e657e3f85ad0a5
isabell-lee08 <isabellleee8@gmail.com>
2025-11-01 16:57:58 -0400

    leaflet map test

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,3 +1,4 @@
+// fetch squirrel data
 async function getSquirrelData(url) {
     try {
         const response = await fetch(url);
@@ -10,3 +11,11 @@ async function getSquirrelData(url) {
 }
 
 getSquirrelData('https://data.cityofnewyork.us/resource/vfnx-vebw.json?$$app_token=IK8DqGYziEQa9OENUbfMRZFzU');
+
+// map
+var map = L.map('map').setView([40.7826, -73.9656], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
